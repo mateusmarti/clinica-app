beec119830983401166f5a371a88e790c14479ed
Mateus Martins <92477926+mateusmarti@users.noreply.github.com>
2026-03-20 17:09:59 -0300

    mais uma correção

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,33 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes.js';
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import authRoutes from './routes/authRoutes.js'
+import consultaRoutes from './routes/consultaRoutes.js'
+import cepRoutes from './routes/cepRoutes.js'
+import climaRoutes from './routes/climaRoutes.js'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://clinica-app-roan.vercel.app'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
-app.use('/api/auth', authRoutes);
+app.use(express.json())
+
+app.use('/api/auth', authRoutes)
+app.use('/api/consultas', consultaRoutes)
+app.use('/api/cep', cepRoutes)
+app.use('/api/clima', climaRoutes)
 
 app.get('/', (req, res) => {
-  res.send('API rodando 🚀');
-});
+  res.send('API rodando 🚀')
+})
 
-export default app;
+export default app
