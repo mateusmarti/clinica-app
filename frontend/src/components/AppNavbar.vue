ee280f3eaed85faac2218f623a85eb8e705c20e0
Mateus Martins <92477926+mateusmarti@users.noreply.github.com>
2026-03-20 17:41:35 -0300

    navbar

--- frontend/src/components/AppNavbar.vue
+++ frontend/src/components/AppNavbar.vue
@@ -15,21 +15,27 @@
       </button>
 
       <div class="collapse navbar-collapse" id="menu">
-        <ul class="navbar-nav ms-auto">
+        <ul class="navbar-nav ms-auto align-items-lg-center gap-lg-2">
           <li class="nav-item" v-if="isLogged">
             <router-link class="nav-link text-white" to="/dashboard">Painel</router-link>
           </li>
+
           <li class="nav-item" v-if="isLogged">
             <router-link class="nav-link text-white" to="/agendar">Agendar</router-link>
           </li>
+
           <li class="nav-item" v-if="!isLogged">
             <router-link class="nav-link text-white" to="/login">Login</router-link>
           </li>
+
           <li class="nav-item" v-if="!isLogged">
             <router-link class="nav-link text-white" to="/register">Cadastro</router-link>
           </li>
+
           <li class="nav-item" v-if="isLogged">
-            <button class="btn btn-outline-light ms-lg-3" @click="logout">Sair</button>
+            <button class="btn btn-outline-light ms-lg-2" @click="logout">
+              Sair
+            </button>
           </li>
         </ul>
       </div>
@@ -38,15 +44,26 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
-import { useRouter } from 'vue-router'
+import { ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
 const router = useRouter()
-const isLogged = computed(() => !!localStorage.getItem('token'))
+const route = useRoute()
+
+const isLogged = ref(!!localStorage.getItem('token'))
+
+watch(
+  () => route.fullPath,
+  () => {
+    isLogged.value = !!localStorage.getItem('token')
+  },
+  { immediate: true }
+)
 
 const logout = () => {
   localStorage.removeItem('token')
   localStorage.removeItem('user')
+  isLogged.value = false
   router.push('/login')
 }
 </script>
